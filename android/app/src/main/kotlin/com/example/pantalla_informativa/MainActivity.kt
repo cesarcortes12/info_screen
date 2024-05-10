dc1a461bfa5eb5531557b54244f0f2f44ff1326f
Cesar Cortes <cesar.cortes@dms.ms>
2024-05-10 18:06:09 -0500

    responsive implementation

---
+++ android/app/src/main/kotlin/com/example/pantalla_informativa/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.pantalla_informativa
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/features/info_screen/presentation/screens/info_screen.dart
@@ -0,0 +1,422 @@
+import 'dart:ui';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter/widgets.dart';
+
+List<Map<String, String>> infoList = [
+  {
+    'placa': 'ABC123',
+    'nombre': 'Cesar Augusto Cortes zapata ',
+    'asignado_a': 'Juan Pérez ',
+    'hora_entrega': '10:00 AM',
+    'estado': 'En orden De taller'
+  },
+  {
+    'placa': 'DEF456',
+    'nombre': 'Jane Smith',
+    'asignado_a': 'María García',
+    'hora_entrega': '11:30 AM',
+    'estado': 'En Ejecucion'
+  },
+  {
+    'placa': 'GHI789',
+    'nombre': 'Alice Johnson',
+    'asignado_a': 'Pedro Rodríguez',
+    'hora_entrega': '1:00 PM',
+    'estado': 'Pausado'
+  },
+  {
+    'placa': 'JKL012',
+    'nombre': 'Bob Brown',
+    'asignado_a': 'Luis Martínez',
+    'hora_entrega': '2:30 PM',
+    'estado': 'facturado'
+  },
+  {
+    'placa': 'MNO345',
+    'nombre': 'Emily Wilson',
+    'asignado_a': 'Carlos López',
+    'hora_entrega': '4:00 PM',
+    'estado': 'listo para entrega'
+  },
+];
+
+/*
+en orden de taller
+en ejecucion
+pausado
+finalizado
+facturado
+listo para entrega
+entregado
+ */
+
+Color getColorForStatus(String state) {
+  switch (state.toLowerCase()) {
+    case 'en orden de taller':
+      return const Color.fromARGB(255, 85, 108, 237);
+
+    case 'en ejecucion':
+      return Color.fromARGB(205, 253, 186, 0);
+
+    case 'pausado':
+      return const Color.fromARGB(255, 243, 81, 81);
+
+    case 'en revision':
+      return Color.fromARGB(255, 77, 242, 236);
+
+    case 'facturado':
+      return Color.fromARGB(255, 243, 255, 5);
+
+    case 'listo para entrega':
+      return Color.fromARGB(255, 79, 215, 0);
+
+    case 'entregado':
+      return Colors.grey;
+
+    default:
+      return Colors.grey;
+  }
+}
+
+class InfoScreen extends StatelessWidget {
+  String title = 'Info Screen';
+
+  InfoScreen({super.key, required this.title});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Center(
+          child: Text(
+            title,
+            style: TextStyle(color: Colors.white),
+          ),
+        ),
+        backgroundColor: Color.fromARGB(255, 4, 67, 160),
+      ),
+      body: Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+        _CardInfo(),
+        _CardInfo2(),
+      ]),
+    );
+  }
+}
+
+class _CardInfo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    return Container(
+      padding: EdgeInsets.all(30),
+      width: size.width * 0.4,
+      child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+        Text('Su Vehiculo',
+            style: TextStyle(
+                fontSize: 40,
+                fontFamily: 'Poppins',
+                fontWeight: FontWeight.bold)),
+        SizedBox(height: 20),
+        Expanded(
+            child: ListView.builder(
+                itemCount: infoList.length,
+                itemBuilder: (context, index) {
+                  final info = infoList[index];
+                  return Container(
+                    margin: EdgeInsets.symmetric(vertical: 10),
+                    width: size.width * 0.4,
+                    decoration: BoxDecoration(
+                      color: Color.fromARGB(255, 4, 67, 160),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Flexible(
+                          flex: 2,
+                          child: Container(
+                            //margin: EdgeInsets.only(right: 103),
+                            //width: size.width * 0.10,
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                ListTile(
+                                    title: Text('Placa : ${info['placa']}',
+                                        style: TextStyle(color: Colors.white)),
+                                    subtitle: Column(
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.start,
+                                        children: [
+                                          SizedBox(height: 6),
+                                          Text('Nombre: ${info['nombre']}',
+                                              style: TextStyle(
+                                                  color: Colors.white)),
+                                          SizedBox(height: 6),
+                                          Text(
+                                              'Asignado a: ${info['asignado_a']}',
+                                              style: TextStyle(
+                                                  color: Colors.white)),
+                                          // SizedBox(height: 6),
+                                          // Text(
+                                          //     'Hora Entrega: ${info['hora_entrega']}',
+                                          //     style: TextStyle(
+                                          //         color: Colors.white)),
+                                        ])),
+                              ],
+                            ),
+                          ),
+                        ),
+                        Flexible(
+                          flex: 1,
+                          child: Container(
+                            height: 116,
+                            decoration: BoxDecoration(
+                              color: Color.fromARGB(255, 255, 255, 255)
+                                  .withOpacity(0.8),
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                            child: _StateCard(
+                              status: info['estado'].toString(),
+                              time: info['hora_entrega'].toString(),
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                  );
+                }))
+      ]),
+    );
+  }
+}
+
+class _CardInfo2 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    return Container(
+        padding: EdgeInsets.all(30),
+        width: size.width * 0.6,
+        child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+          Text('Proximo a Entregar',
+              style: TextStyle(
+                  fontSize: 40,
+                  fontFamily: 'Poppins',
+                  fontWeight: FontWeight.bold)),
+          SizedBox(height: 20),
+          Stack(children: [
+            Container(
+              width: size.width * 0.9,
+              height: 500,
+              decoration: BoxDecoration(
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.withOpacity(0.5),
+                    spreadRadius: 5,
+                    blurRadius: 7,
+                    offset: Offset(0, 3),
+                  )
+                ],
+                image: DecorationImage(
+                    image: AssetImage(
+                        'assets/images/vehiculo_frontal_derecha.jpg'),
+                    fit: BoxFit.cover),
+                borderRadius: BorderRadius.circular(10),
+              ),
+            ),
+            Positioned(
+              left: size.width * 0.02,
+              top: size.height * 0.15,
+              child: Container(
+                width: size.width * 0.25,
+                height: size.width * 0.15,
+                decoration: BoxDecoration(
+                  color: Color.fromARGB(255, 255, 255, 255).withOpacity(0.8),
+                  borderRadius: BorderRadius.circular(10),
+                  boxShadow: [
+                    BoxShadow(
+                      color:
+                          const Color.fromARGB(255, 0, 0, 0).withOpacity(0.4),
+                      spreadRadius: 3,
+                      blurRadius: 5,
+                      offset: Offset(0, 3),
+                    ),
+                  ],
+                ),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(10),
+                  child: BackdropFilter(
+                    filter: ImageFilter.blur(sigmaX: 3, sigmaY: 3),
+                    child: LayoutBuilder(
+                      builder: (context, constraints) {
+                        return Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              'PLACA',
+                              style: TextStyle(
+                                fontSize: constraints.maxWidth *
+                                    0.2, // Ajuste dinámico del tamaño del texto
+                                fontWeight: FontWeight.bold,
+                                color: Color(0xFF000000),
+                              ),
+                            ),
+                            Container(
+                              margin: EdgeInsets.symmetric(horizontal: 10),
+                              child: Text(
+                                infoList[0]['placa'] ?? '',
+                                style: TextStyle(
+                                  fontSize: constraints.maxWidth *
+                                      0.20, // Ajuste dinámico del tamaño del texto
+                                  fontWeight: FontWeight.bold,
+                                  color: Color.fromARGB(255, 4, 67, 160),
+                                ),
+                              ),
+                            ),
+                          ],
+                        );
+                        
+                      },
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            Positioned(
+              right: 20,
+              top: 130,
+              child: Container(
+                width: size.width * 0.25,
+                height: 280,
+                decoration: BoxDecoration(
+                  color: Color.fromARGB(255, 255, 255, 255).withOpacity(0.8),
+                  borderRadius: BorderRadius.circular(10),
+                  boxShadow: [
+                    BoxShadow(
+                      color:
+                          const Color.fromARGB(255, 0, 0, 0).withOpacity(0.5),
+                      spreadRadius: 3,
+                      blurRadius: 5,
+                      offset: Offset(0, 3),
+                    ),
+                  ],
+                ),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(10),
+                  child: BackdropFilter(
+                    filter: ImageFilter.blur(sigmaX: 3, sigmaY: 3),
+                    child: Center(
+                        child: Column(
+                      //mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Container(
+                          margin: EdgeInsets.symmetric(horizontal: 10),
+                          child: FittedBox(
+                            fit: BoxFit.scaleDown,
+                            child: Text(
+                              'PROPIETARIO',
+                              style: TextStyle(
+                                fontSize: 50,
+                                fontWeight: FontWeight.bold,
+                                color: Color(0xFF000000),
+                              ),
+                            ),
+                          ),
+                        ),
+                        Container(
+                          margin: EdgeInsets.symmetric(horizontal: 10),
+                          width: size.width * 1.0,
+                          height: 200,
+                          child: Center(
+                            child: LimitedBox(
+                              maxWidth: 100,
+                              maxHeight: 250,
+                              child: Text(
+                                  textAlign: TextAlign.center,
+                                  '${infoList[0]['nombre']}',
+                                  maxLines: 2,
+                                  overflow: TextOverflow.ellipsis,
+                                  style: TextStyle(
+                                    fontSize: 60,
+                                    fontWeight: FontWeight.bold,
+                                    color: Color.fromARGB(255, 4, 67, 160),
+                                  )),
+                            ),
+                          ),
+                        ),
+                      ],
+                    )),
+                  ),
+                ),
+              ),
+            ),
+          ])
+        ]));
+  }
+}
+
+class _StateCard extends StatelessWidget {
+  final String status;
+  final String time;
+  //final Map<String, String> info;
+
+  const _StateCard({
+    required this.status,
+    Key? key,
+    required this.time,
+  }) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          width: double.infinity,
+          height: 40,
+          decoration: BoxDecoration(
+            color: getColorForStatus(status),
+            borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(10), topRight: Radius.circular(10)),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.only(top: 10),
+            child: Text(
+              status,
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: Colors.black,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(
+            top: 15,
+          ),
+          child: Container(
+            child: Column(
+              children: [
+                Text(
+                  'Hora de entrega',
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold, color: Colors.black),
+                ),
+                Text(
+                  time,
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 25,
+                      fontWeight: FontWeight.bold),
+                ),
+              ],
+            ),
+          ),
+        )
+      ],
+    );
+  }
+}
